a22d102f60bbc33ca23a06d019ccfec8cd1b9158
hxy114 <2248020708@qq.com>
2024-10-18 00:16:25 +0800

    适配服务器matrixkv

---
+++ test_matrixkv.sh
@@ -0,0 +1,54 @@
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/matrixkv.properties -s " >> matrixkv_ssd.log
+sudo ./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/matrixkv.properties -s >>matrixkv_ssd.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/matrixkv.properties -s " >> matrixkv_ssd.log
+sudo ./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/matrixkv.properties -s >>matrixkv_ssd.log
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/matrixkv.properties -s " >> matrixkv_ssd.log
+sudo ./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/matrixkv.properties -s >>matrixkv_ssd.log
+
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/matrixkv.properties -s " >> matrixkv_ssd.log
+sudo ./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/matrixkv.properties -s >>matrixkv_ssd.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/matrixkv.properties -s " >> matrixkv_ssd.log
+sudo ./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/matrixkv.properties -s >>matrixkv_ssd.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/matrixkv.properties -s " >> matrixkv_ssd.log
+sudo ./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/matrixkv.properties -s >>matrixkv_ssd.log
