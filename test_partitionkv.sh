fe3ffa4dc325ab61209c9d4527ee518f9db0c89c
hxy114 <2248020708@qq.com>
2024-10-17 17:24:55 +0800

    适配服务器partitionkv

--- test_partitionkv.sh
+++ test_partitionkv.sh
@@ -1,54 +1,54 @@
-ulimit  -n 10000
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloada -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
-./ycsb -load -run -db leveldb -P workloads/workloada -P leveldb/leveldb.properties -s >>partitionkv_ssd_kv.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloada -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
+.sudo /ycsb -load -run -db leveldb -P workloads/workloada -P partition-r/rocksdb.properties -s >>partitionkv_ssd_kv.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
-./ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s >>partitionkv_ssd_kv.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
+sudo ./ycsb -load -run -db leveldb -P workloads/workloadb -P partition-r/rocksdb.properties -s >>partitionkv_ssd_kv.log
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadc -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
-./ycsb -load -run -db leveldb -P workloads/workloadc -P leveldb/leveldb.properties -s >>partitionkv_ssd_kv.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadc -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
+sudo ./ycsb -load -run -db leveldb -P workloads/workloadc -P partition-r/rocksdb.properties-s >>partitionkv_ssd_kv.log
 
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadd -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
-./ycsb -load -run -db leveldb -P workloads/workloadd -P leveldb/leveldb.properties -s >>partitionkv_ssd_kv.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadd -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
+sudo ./ycsb -load -run -db leveldb -P workloads/workloadd -P partition-r/rocksdb.properties -s >>partitionkv_ssd_kv.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloade -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
-./ycsb -load -run -db leveldb -P workloads/workloade -P leveldb/leveldb.properties -s >>partitionkv_ssd_kv.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloade -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
+sudo ./ycsb -load -run -db leveldb -P workloads/workloade -P partition-r/rocksdb.properties -s >>partitionkv_ssd_kv.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadf -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
-./ycsb -load -run -db leveldb -P workloads/workloadf -P leveldb/leveldb.properties -s >>partitionkv_ssd_kv.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadf -P leveldb/leveldb.properties -s " >> partitionkv_ssd_kv.log
+sudo ./ycsb -load -run -db leveldb -P workloads/workloadf -P partition-r/rocksdb.properties -s >>partitionkv_ssd_kv.log
