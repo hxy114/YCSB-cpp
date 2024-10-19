f6b37921250e3eb4d387fa06e1752c51ffa23dd4
hxy114 <2248020708@qq.com>
2024-10-19 20:59:34 +0800

    适配novelsm 和rocksdb

--- test_rocksdb.sh
+++ test_rocksdb.sh
@@ -5,7 +5,7 @@ sudo rm -rf /mnt/pmemdir/pm_log
 sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
 sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/matrixkv.properties -s >>rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/rocksdb.properties -s >>rocksdb.log
 
 
 sudo rm -rf /mnt/data_02/dbbench
@@ -14,7 +14,7 @@ sudo rm -rf /mnt/pmemdir/pm_log
 sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
 sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/matrixkv.properties -s >>rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/rocksdb.properties -s >>rocksdb.log
 
 
 sudo rm -rf /mnt/data_02/dbbench
@@ -23,7 +23,7 @@ sudo rm -rf /mnt/pmemdir/pm_log
 sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
 sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/matrixkv.properties -s >>rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/rocksdb.properties -s >>rocksdb.log
 
 
 sudo rm -rf /mnt/data_02/dbbench
@@ -32,7 +32,7 @@ sudo rm -rf /mnt/pmemdir/pm_log
 sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
 sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/matrixkv.properties -s >>rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/rocksdb.properties -s >>rocksdb.log
 
 
 sudo rm -rf /mnt/data_02/dbbench
@@ -41,7 +41,7 @@ sudo rm -rf /mnt/pmemdir/pm_log
 sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
 sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/matrixkv.properties -s >>rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/rocksdb.properties -s >>rocksdb.log
 
 
 sudo rm -rf /mnt/data_02/dbbench
@@ -49,7 +49,7 @@ sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
 sudo rm -rf /mnt/pmemdir/pm_log
 sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/matrixkv.properties -s >>rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/rocksdb.properties -s >>rocksdb.log
 
 
 sudo rm -rf /mnt/data_02/dbbench
