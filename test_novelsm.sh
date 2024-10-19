a4270092402d799ad33d130e2597b9feb6b0e34c
hxy114 <2248020708@qq.com>
2024-10-19 18:38:17 +0800

    适配novelsm 和rocksdb

--- test_novelsm.sh
+++ test_novelsm.sh
@@ -1,65 +1,65 @@
 ulimit  -n 10000
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -fr /mnt/pmemdir/*map
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -fr /mnt/pmemdir/*map
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db novelsm -P workloads/workloada -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
-./ycsb -load -run -db novelsm -P workloads/workloada -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
+sudo echo "./ycsb -load -run -db novelsm -P workloads/workloada -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db novelsm -P workloads/workloada -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-rm -fr /mnt/pmemdir/*map
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo rm -fr /mnt/pmemdir/*map
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db novelsm -P workloads/workloadb -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
-./ycsb -load -run -db novelsm -P workloads/workloadb -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
+sudo echo "./ycsb -load -run -db novelsm -P workloads/workloadb -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db novelsm -P workloads/workloadb -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-rm -fr /mnt/pmemdir/*map
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo rm -fr /mnt/pmemdir/*map
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db novelsm -P workloads/workloadc -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
-./ycsb -load -run -db novelsm -P workloads/workloadc -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
+sudo echo "./ycsb -load -run -db novelsm -P workloads/workloadc -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db novelsm -P workloads/workloadc -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-rm -fr /mnt/pmemdir/*map
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo rm -fr /mnt/pmemdir/*map
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db novelsm -P workloads/workloadd -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
-./ycsb -load -run -db novelsm -P workloads/workloadd -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
+sudo echo "./ycsb -load -run -db novelsm -P workloads/workloadd -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db novelsm -P workloads/workloadd -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-rm -fr /mnt/pmemdir/*map
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo rm -fr /mnt/pmemdir/*map
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db novelsm -P workloads/workloade -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
-./ycsb -load -run -db novelsm -P workloads/workloade -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
+sudo echo "./ycsb -load -run -db novelsm -P workloads/workloade -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db novelsm -P workloads/workloade -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-rm -fr /mnt/pmemdir/*map
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo rm -fr /mnt/pmemdir/*map
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db novelsm -P workloads/workloadf -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
-./ycsb -load -run -db novelsm -P workloads/workloadf -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
+sudo echo "./ycsb -load -run -db novelsm -P workloads/workloadf -P novelsmdb/novelsmdb.properties -s " >> novelsmdb_ssd.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db novelsm -P workloads/workloadf -P novelsmdb/novelsmdb.properties -s >>novelsmdb_ssd.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-rm -fr /mnt/pmemdir/*map
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo rm -fr /mnt/pmemdir/*map
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
--- test_rocksdb.sh
+++ test_rocksdb.sh
@@ -1,29 +1,58 @@
 
-rm -rf /tmp/ycsb-rocksdb/
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /tmp/ycsb-leveldb/
-rm -rf /mnt/pmemdir/pm_log
-echo "./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/matrixkv.properties -s >>rocksdb.log
-rm -rf /tmp/ycsb-rocksdb/
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-echo "./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/matrixkv.properties -s >>rocksdb.log
-rm -rf /tmp/ycsb-rocksdb/
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-echo "./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/matrixkv.properties -s >>rocksdb.log
-rm -rf /tmp/ycsb-rocksdb/
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-echo "./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/matrixkv.properties -s >>rocksdb.log
-rm -rf /tmp/ycsb-rocksdb/
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-echo "./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/matrixkv.properties -s >>rocksdb.log
-rm -rf /tmp/ycsb-rocksdb/
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-echo "./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/rocksdb.properties -s " >> rocksdb.log
-./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/matrixkv.properties -s >>rocksdb.log
-rm -rf /tmp/ycsb-rocksdb/
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/rocksdb.properties -s " >> rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/matrixkv.properties -s >>rocksdb.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/rocksdb.properties -s " >> rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/matrixkv.properties -s >>rocksdb.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/rocksdb.properties -s " >> rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/matrixkv.properties -s >>rocksdb.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/rocksdb.properties -s " >> rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/matrixkv.properties -s >>rocksdb.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/rocksdb.properties -s " >> rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/matrixkv.properties -s >>rocksdb.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/rocksdb.properties -s " >> rocksdb.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/matrixkv.properties -s >>rocksdb.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
