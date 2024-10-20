9ff0bb4abdde98f44f82b2d6830dc93d36b152d0
hxy114 <2248020708@qq.com>
2024-10-20 13:50:46 +0800

    适配flat和kv分离partitionkv

--- test_flat.sh
+++ test_flat.sh
@@ -1,54 +1,54 @@
-ulimit  -n 10000
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloada -P leveldb/leveldb.properties -s " >> flatlsm_ssd.log
-./ycsb -load -run -db leveldb -P workloads/workloada -P leveldb/leveldb.properties -s >>flatlsm_ssd.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloada -P leveldb/leveldb.properties -s " >> flatlsm_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloada -P partition-r/flatlsm.properties -s >>flatlsm_ssd_kv.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s " >> flatlsm_ssd.log
-./ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s >>flatlsm_ssd.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s " >> flatlsm_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadb -P partition-r/flatlsm.properties -s >>flatlsm_ssd_kv.log
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadc -P leveldb/leveldb.properties -s " >> flatlsm_ssd.log
-./ycsb -load -run -db leveldb -P workloads/workloadc -P leveldb/leveldb.properties -s >>flatlsm_ssd.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadc -P leveldb/leveldb.properties -s " >> flatlsm_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadc -P partition-r/flatlsm.properties -s >>flatlsm_ssd_kv.log
 
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadd -P leveldb/leveldb.properties -s " >> flatlsm_ssd.log
-./ycsb -load -run -db leveldb -P workloads/workloadd -P leveldb/leveldb.properties -s >>flatlsm_ssd.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadd -P leveldb/leveldb.properties -s " >> flatlsm_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadd -P partition-r/flatlsm.properties -s >>flatlsm_ssd_kv.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloade -P leveldb/leveldb.properties -s " >> flatlsm_ssd.log
-./ycsb -load -run -db leveldb -P workloads/workloade -P leveldb/leveldb.properties -s >>flatlsm_ssd.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloade -P leveldb/leveldb.properties -s " >> flatlsm_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloade -P partition-r/flatlsm.properties -s >>flatlsm_ssd_kv.log
 
 
-rm -rf /run/media/root/pci-ssd/dbbench
-rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
-rm -rf /mnt/pmemdir/pm_log
-echo 3 > /proc/sys/vm/drop_caches
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
 sleep 60
-echo "./ycsb -load -run -db leveldb -P workloads/workloadf -P leveldb/leveldb.properties -s " >> flatlsm_ssd.log
-./ycsb -load -run -db leveldb -P workloads/workloadf -P leveldb/leveldb.properties -s >>flatlsm_ssd.log
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadf -P leveldb/leveldb.properties -s " >> flatlsm_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadf -P partition-r/flatlsm.properties -s >>flatlsm_ssd_kv.log
---
+++ test_partitionkv_kv.sh
@@ -0,0 +1,54 @@
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloada -P leveldb/leveldb.properties -s " >> partitionkv_kv_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloada -P partition-r/partitionkv_kv.properties -s >>partitionkv_kv_ssd_kv.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadb -P leveldb/leveldb.properties -s " >> partitionkv_kv_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadb -P partition-r/partitionkv_kv.properties -s >>partitionkv_kv_ssd_kv.log
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadc -P leveldb/leveldb.properties -s " >> partitionkv_kv_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadc -P partition-r/partitionkv_kv.properties -s >>partitionkv_kv_ssd_kv.log
+
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadd -P leveldb/leveldb.properties -s " >> partitionkv_kv_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadd -P partition-r/partitionkv_kv.properties -s >>partitionkv_kv_ssd_kv.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloade -P leveldb/leveldb.properties -s " >> partitionkv_kv_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloade -P partition-r/partitionkv_kv.properties -s >>partitionkv_kv_ssd_kv.log
+
+
+sudo rm -rf /mnt/data_02/dbbench
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db leveldb -P workloads/workloadf -P leveldb/leveldb.properties -s " >> partitionkv_kv_ssd_kv.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db partition-r -P workloads/workloadf -P partition-r/partitionkv_kv.properties -s >>partitionkv_kv_ssd_kv.log
