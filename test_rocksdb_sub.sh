243bb2e5654a0f4967522aff5f044797acff32bd
hxy114 <2248020708@qq.com>
2025-03-11 09:40:38 +0800

    适配rocksdb的子压缩

---
+++ test_rocksdb_sub.sh
@@ -0,0 +1,58 @@
+
+sudo rm -rf /mnt/data_02/*
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/rocksdb.properties -s " >> rocksdb_sub.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloada -P rocksdb/rocksdb_sub_compaction.properties -s >>rocksdb_sub.log
+
+
+sudo rm -rf /mnt/data_02/*
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/rocksdb.properties -s " >> rocksdb_sub.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadb -P rocksdb/rocksdb_sub_compaction.properties -s >>rocksdb_sub.log
+
+
+sudo rm -rf /mnt/data_02/*
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/rocksdb.properties -s " >> rocksdb_sub.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadc -P rocksdb/rocksdb_sub_compaction.properties -s >>rocksdb_sub.log
+
+
+sudo rm -rf /mnt/data_02/*
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/rocksdb.properties -s " >> rocksdb_sub.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadd -P rocksdb/rocksdb_sub_compaction.properties -s >>rocksdb_sub.log
+
+
+sudo rm -rf /mnt/data_02/*
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sleep 60
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/rocksdb.properties -s " >> rocksdb_sub.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloade -P rocksdb/rocksdb_sub_compaction.properties -s >>rocksdb_sub.log
+
+
+sudo rm -rf /mnt/data_02/*
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
+sudo echo "./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/rocksdb.properties -s " >> rocksdb_sub.log
+numactl --cpunodebind=1 --membind=1 sudo ./ycsb -load -run -db rocksdb -P workloads/workloadf -P rocksdb/rocksdb_sub_compaction.properties -s >>rocksdb_sub.log
+
+
+sudo rm -rf /mnt/data_02/*
+sudo rm -fr /mnt/pmemdir/cf_0_default_sstable.pool
+sudo rm -rf /mnt/pmemdir/pm_log
+sudo sh -c "/usr/bin/echo 3 > /proc/sys/vm/drop_caches"
